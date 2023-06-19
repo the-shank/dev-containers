78078d861e6d3ee5642357b74892d42717e46418
Shank <shashanksharma21@gmail.com>
2023-06-19 09:05:17 -0400

    add separate build an run scripts

---
+++ docker_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $1 == "" ]] || [[ $2 == "" ]]; then
+  echo "[!] Usage: docker_build.sh <password> <image_name>"
+  exit 1
+fi
+
+pass="$1"
+image_name="$2"
+
+docker build \
+  --no-cache \
+  --build-arg PASSWD="${pass}" \
+  --build-arg="UID=(id -u)" \
+  --build-arg="GID=(id -g)" \
+  -t "${image_name}" \
+  -f Dockerfile2204
+  .
---
+++ docker_run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# =============================================================
+#
+# Usage:
+#
+# For first time container:
+# docker_run.sh <container_name> <hostname> <image_name>
+# 
+# For second time onwards:
+# docker_run.sh <container_name>
+#
+# =============================================================
+
+usage(){
+  echo "[!] Usage: "
+  echo "For creating and running container:"
+  echo ">> docker_run.sh <container_name> <hostname> <image_name>"
+  echo "For running existing container:"
+  echo ">> docker_run.sh <container_name>"
+}
+
+if [[ $1 == "" ]]; then
+  usage
+  exit 1
+fi
+
+container_name=$1
+
+if [[ $2 == "" ]] && [[ $3 == "" ]]; then
+  # re-running existing container
+  # TODO
+  
+else
+  hostname=$2
+  image_name=$3
+
+  docker run \
+    --label no-prune \
+    -it \
+    --name "$container_name" \
+    --hostname "$hostname" \
+    --volume $HOME:/host_home \
+    "$image_name"
+fi
+
