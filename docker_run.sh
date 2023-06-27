52c23c450102c6ab5d7dcaf230c63dea8a5c655d
Shank <shashanksharma21@gmail.com>
2023-06-27 18:53:18 -0400

    save image_name and container_name for future use

--- docker_run.sh
+++ docker_run.sh
@@ -15,9 +15,20 @@
 usage(){
   echo "[!] Usage: "
   echo "For creating and running container:"
-  echo ">> docker_run.sh <container_name> <hostname> <image_name>"
+  echo ">> docker_run.sh <container_name> <hostname> [<image_name>]"
+  echo ">> if <image_name> is not provided, it defaults to the one in ./.image_name"
   echo "For running existing container:"
-  echo ">> docker_run.sh <container_name>"
+  echo ">> docker_run.sh [<container_name>]"
+  echo ">> if <container_name> is not provided, it defaults to the one in ./.container_name"
+}
+
+# Usage:
+# ensure_not_empty <val> <msg>
+ensure_not_empty() {
+  if [[ $1 == "" ]]; then
+    echo "${msg} was empty"
+    exit 1
+  fi
 }
 
 if [[ $1 == "" ]]; then
@@ -36,6 +47,16 @@ if [[ $2 == "" ]] && [[ $3 == "" ]]; then
 else
   hostname=$2
   image_name=$3
+  if [[ $image_name == "" ]]; then
+    image_name_from_file=$(cat .image_name)
+    if [[ $image_name_from_file == "" ]]; then
+      echo "[!] no image_name provided and ./.image_name is also empty. Please provide a image_name"
+      exit 1
+    fi
+    image_name=$image_name_from_file
+  fi
+
+  echo "${container_name}" > ./.container_name
 
   docker run \
     --label no-prune \
