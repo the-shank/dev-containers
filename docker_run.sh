342f413cdd4fe2c5bcd2666cbdf174dbaf97b403
Shank <shashanksharma21@gmail.com>
2023-06-27 21:13:37 -0400

    fix run command to accept additional args (like additional volumes)

--- docker_run.sh
+++ docker_run.sh
@@ -15,12 +15,16 @@
 usage(){
   echo "[!] Usage: "
   echo "For creating and running container:"
-  echo ">> docker_run.sh <container_name> <hostname> [<image_name>]"
-  echo ">> if <image_name> is not provided, it defaults to the one in ./.image_name"
+  echo ">> docker_run.sh <container_name> <image_name> [<other_args>..]"
   echo "For running existing container:"
   echo ">> docker_run.sh [<container_name>]"
   echo ">> if <container_name> is not provided, it defaults to the one in ./.container_name"
 }
+
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+  usage
+  exit 0
+fi
 
 # Usage:
 # ensure_not_empty <val> <msg>
@@ -32,8 +36,7 @@ ensure_not_empty() {
 }
 
 container_name=$1
-
-if [[ $1 == "" ]] || ( [[ $2 == "" ]] && [[ $3 == "" ]] ); then
+if [[ $1 == "" ]]; then
   # re-running existing container
   if [[ ${container_name} == "" ]]; then
     container_name_from_file=$(cat .container_name)
@@ -48,16 +51,17 @@ if [[ $1 == "" ]] || ( [[ $2 == "" ]] && [[ $3 == "" ]] ); then
   
 else
   # creating new container
-  hostname=$2
-  image_name=$3
+  hostname="$container_name"
+
+  image_name=$2
   if [[ $image_name == "" ]]; then
-    image_name_from_file=$(cat .image_name)
-    if [[ $image_name_from_file == "" ]]; then
-      echo "[!] no image_name provided and ./.image_name is also empty. Please provide a image_name"
-      exit 1
-    fi
-    image_name=$image_name_from_file
+    echo "[!] no image_name provided. Please provide a image_name"
+    exit 1
   fi
+
+  # echo ">> hostname: $hostname"
+  # echo ">> image_name: $image_name"
+  # echo ">> container_name: $container_name"
 
   echo "${container_name}" > ./.container_name
 
@@ -69,5 +73,6 @@ else
     --volume $HOME:/host_home \
     --volume $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
     --env SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+    "${@:3}" \
     "$image_name"
 fi
