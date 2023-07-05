e1aa4b64f3de4f2c773a6c1a7a2075229ba8139d
Shank <shashanksharma21@gmail.com>
2023-07-05 00:30:17 -0400

    add `--no-cache` option to build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# NOTE:
+# $3 can be used to pass in `--no-cache` to create the image from scratch
+
 if [[ $1 == "" ]] || [[ $2 == "" ]]; then
   echo "[!] Usage: docker_build.sh <password> <image_name>"
   exit 1
@@ -9,12 +12,12 @@ pass="$1"
 image_name="$2"
 
 docker build \
-  --no-cache \
+  $3 \
   --build-arg PASSWD="${pass}" \
   --build-arg="UID=$(id -u)" \
   --build-arg="GID=$(id -g)" \
   -t "${image_name}" \
-  -f Dockerfile2204 .
+  -f Dockerfile.voidlinux .
 
 # save the image name for future usage
 echo "${image_name}" > ./.image_name
