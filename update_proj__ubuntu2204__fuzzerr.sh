2d311082b7c205a737c3b6c197e5d0a7638fccca
Shank <shashanksharma21@gmail.com>
2023-10-24 15:26:48 -0400

    fuzzer | add container for fuzzerr

---
+++ update_proj__ubuntu2204__fuzzerr.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -eu
+set -o pipefail
+
+# params - base
+BASE_IMAGENAME=devc-ubuntu2204-fuzzerr:latest
+BASE_DOCKERFILE=Dockerfile.ubuntu2204.fuzzerr
+BASE_CONTAINERNAME=devc-ubuntu2204-fuzzerr
+
+DEVC_REPO_DIR=/home/shank/code/misc/dev-container
+${DEVC_REPO_DIR}/update_base.sh $BASE_IMAGENAME $BASE_DOCKERFILE $BASE_CONTAINERNAME ${@:1}
