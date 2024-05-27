c35e2800772425b826872b309be89c6e86873193
Shank <shashanksharma21@gmail.com>
2024-05-27 11:58:32 -0400

    rename autonostd -> aunor

--- update_proj__archlinux__aunor.sh
+++ update_proj__archlinux__aunor.sh
@@ -4,9 +4,9 @@ set -eu
 set -o pipefail
 
 # params - base
-BASE_IMAGENAME=devc-archlinux-autonostd:base-devel
-BASE_DOCKERFILE=Dockerfile.archlinux.autonostd
-BASE_CONTAINERNAME=devc-archlinux-autonostd
+BASE_IMAGENAME=devc-archlinux-aunor:base-devel
+BASE_DOCKERFILE=Dockerfile.archlinux.aunor
+BASE_CONTAINERNAME=devc-archlinux-aunor
 
 DEVC_REPO_DIR=/home/shank/code/misc/dev-container
 ${DEVC_REPO_DIR}/update_base.sh $BASE_IMAGENAME $BASE_DOCKERFILE $BASE_CONTAINERNAME ${@:1}
