b38bb05febed6502b649be9122c2e157470991ba
Shank <shashanksharma21@gmail.com>
2026-02-10 09:25:12 -0500

    aunor | move to podman

--- update_proj__archlinux__aunor.sh
+++ update_proj__archlinux__aunor.sh
@@ -4,8 +4,8 @@ set -eu
 set -o pipefail
 
 # params - base
-BASE_IMAGENAME=devc-archlinux-aunor:base-devel
-BASE_DOCKERFILE=Dockerfile.archlinux.aunor
+BASE_IMAGENAME=localhost/devc-archlinux-aunor:base-devel
+BASE_DOCKERFILE=Containerfile.archlinux.aunor
 BASE_CONTAINERNAME=devc-archlinux-aunor
 
 DEVC_REPO_DIR=/home/shank/code/misc/dev-container
