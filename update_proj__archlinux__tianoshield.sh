0732be565de11bc650f9586b9e0efb90dcb3ac2f
Shank <shashanksharma21@gmail.com>
2026-02-19 20:06:52 -0500

    tianoshield | fix container name

--- update_proj__archlinux__tianoshield.sh
+++ update_proj__archlinux__tianoshield.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # params - base
 BASE_IMAGENAME=devc-archlinux-aunor:base-devel
 BASE_DOCKERFILE=Dockerfile.archlinux.tianoshield
-BASE_CONTAINERNAME=devc-archlinux-aunor
+BASE_CONTAINERNAME=devc-archlinux-tianoshield
 
 DEVC_REPO_DIR=/home/shank/code/misc/dev-container
 ${DEVC_REPO_DIR}/update_base.sh $BASE_IMAGENAME $BASE_DOCKERFILE $BASE_CONTAINERNAME ${@:1}
